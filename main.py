93f65ea587b1adc2fea1d8fdc16839d91af2a203
Niko Rutherford <zgithub@isomorphism.ca>
2024-02-17 17:44:56 -0800

    Update template to newest version

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import subprocess
 
 import logging
 
+# The decky plugin module is located at decky-loader/plugin
+# For easy intellisense checkout the decky-loader code one directory up
+# or add the `decky-loader/plugin` path to `python.analysis.extraPaths` in `.vscode/settings.json`
+import decky_plugin
+
+
 logging.basicConfig(
     filename="/tmp/decky-dictation.log",
     format="Decky Dictation: %(asctime)s %(levelname)s %(message)s",
