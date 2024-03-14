d887ee6bf1b32bca451d16ffff21677d29264da9
cboiangiu <catalin.boiangiu@lansoft.ro>
2024-03-14 22:57:42 +0200

    various fixes

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ logger.setLevel(logging.DEBUG)
 std_out_file = open("/tmp/decky-dictation-std-out.log", "w")
 std_err_file = open("/tmp/decky-dictation-std-err.log", "w")
 
-os.environ['PYTHONPATH'] = "/home/deck/homebrew/plugins/decky-dictation/bin/vosk"
+plugin_path = os.environ['DECKY_PLUGIN_DIR']
+os.environ['PYTHONPATH'] = f"{plugin_path}/bin/vosk"
 os.environ["XDG_RUNTIME_DIR"] = "/run/user/1000"
 os.environ["XDG_SESSION_TYPE"] = "wayland"
 os.environ["DISPLAY"] = ":1" # FIXME: the steam ui seems to be 0 and the actual game 1. can we detect current window in focus maybe?
@@ -31,7 +32,7 @@ class Plugin:
     async def begin(self):
         try:
             logger.info("Begin dictation")
-            subprocess.Popen("/home/deck/homebrew/plugins/decky-dictation/bin/nerd-dictation/nerd-dictation begin --vosk-model-dir=/home/deck/homebrew/plugins/decky-dictation/bin/nerd-dictation/model --numbers-min-value 2 --numbers-no-suffix --full-sentence --numbers-as-digits --numbers-use-separator --timeout 4 --punctuate-from-previous-timeout 2 &", shell=True, stdout=std_out_file, stderr=std_err_file)
+            subprocess.Popen(f"{plugin_path}/bin/nerd-dictation/nerd-dictation begin --vosk-model-dir={plugin_path}/bin/nerd-dictation/model --numbers-min-value 2 --numbers-no-suffix --full-sentence --numbers-as-digits --numbers-use-separator --timeout 4 --punctuate-from-previous-timeout 2 &", shell=True, stdout=std_out_file, stderr=std_err_file)
         except Exception:
             await Plugin.end(self)
             logger.info(traceback.format_exc())
@@ -41,7 +42,7 @@ class Plugin:
     async def end(self):
         try:
             logger.info("End dictation")
-            subprocess.Popen("/home/deck/homebrew/plugins/decky-dictation/bin/nerd-dictation/nerd-dictation end", shell=True, stdout=std_out_file, stderr=std_err_file)
+            subprocess.Popen(f"{plugin_path}/bin/nerd-dictation/nerd-dictation end", shell=True, stdout=std_out_file, stderr=std_err_file)
         except Exception:
             logger.info(traceback.format_exc())
         return
