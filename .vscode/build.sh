67bfa9cc614fea08e1de2674475984bec67837f9
cboiangiu <catalin.boiangiu@lansoft.ro>
2024-03-14 20:31:08 +0200

    fix building on macOS

--- .vscode/build.sh
+++ .vscode/build.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 CLI_LOCATION="$(pwd)/cli"
 echo "Building plugin in $(pwd)"
-printf "Please input sudo password to proceed.\n"
 
-# read -s sudopass
+SYSTEM_ARCH="$(uname -a)"
 
-# printf "\n"
+if [[ "$SYSTEM_ARCH" =~ "Darwin" ]]; then
+    $CLI_LOCATION/decky plugin build $(pwd)
+else
+    printf "Please input sudo password to proceed.\n"
 
-echo $sudopass | sudo $CLI_LOCATION/decky plugin build $(pwd)
+    # read -s sudopass
+
+    # printf "\n"
+
+    echo $sudopass | sudo $CLI_LOCATION/decky plugin build $(pwd)
+fi
